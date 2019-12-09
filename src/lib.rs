67388549144f82cda4ecc58d8adafb54fcba3391
freesig <tomrgowan@gmail.com>
2019-12-09 14:31:21 +1100

    cache root

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,15 @@ use hdk::{
 use serde_derive::{Serialize, Deserialize};
 
 use hdk::prelude::*;
+use lazy_static::lazy_static;
+use std::sync::RwLock;
 
 pub mod defs;
 pub use defs::*;
 
 const ROOT_ANCHOR: &'static str = concat!("holochain_anchors", "::", "root_anchor"); 
 const ROOT_ANCHOR_TYPE: &'static str = concat!("holochain_anchors", "::", "RootAnchor"); 
-pub const ANCHOR_TYPE: &'static str = "Anchor";
+pub const ANCHOR_TYPE: &'static str = concat!("holochain_anchors", "::", "Anchor");
 const ROOT_ANCHOR_LINK_TO: &'static str = "anchors";
 
 #[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
@@ -29,22 +31,38 @@ struct Anchor {
 
 type RootAnchor = String;
 
-// Will only create a new root anchor if the Agent is not synched or its first hardTimeout
-// will accumulate headers for each agent that adds.
-// Ideally it would be cool to be able to linkn to the root_anchor_entry.address() and it not be an entry.
+lazy_static!{
+    static ref ROOT_ANCHOR_ADDRESS: RwLock<Option<Address>> = RwLock::new(None);
+}
+
 fn root_anchor() -> ZomeApiResult<Address> {
-    // Creating the root anchor
-    let root_anchor_entry = Entry::App(
-        ROOT_ANCHOR_TYPE.into(),
-        ROOT_ANCHOR.into(),
-    );
-    // Only commit the entry if it doesn't exist from this agents perspective.
-    let root_anchor_entry_address = root_anchor_entry.address();
-    if hdk::get_entry(&root_anchor_entry_address)?.is_none() {
-        Ok(hdk::commit_entry(&root_anchor_entry)?)
-    } else {
-        Ok(root_anchor_entry_address)
+    let root_address = {
+        ROOT_ANCHOR_ADDRESS.read().map_err(|e| ZomeApiError::Internal(format!("Lockfailed {:?}", e)))?.as_ref().map(|a|a.clone())
+    };
+
+    match root_address {
+        None => {
+            let mut lock = ROOT_ANCHOR_ADDRESS.write().map_err(|e| ZomeApiError::Internal(format!("Lockfailed {:?}", e)))?;
+            // Creating the root anchor
+            let root_anchor_entry = Entry::App(
+                ROOT_ANCHOR_TYPE.into(),
+                ROOT_ANCHOR.into(),
+            );
+            // Only commit the entry if it doesn't exist from this agents perspective.
+            let root_anchor_entry_address = root_anchor_entry.address();
+            // get_entry is a slow network call.
+            if let Ok(None) = hdk::get_entry(&root_anchor_entry_address) {
+                let address;
+                address = hdk::commit_entry(&root_anchor_entry)?;
+                *lock = Some(address.clone());
+                Ok(address)
+            } else {
+                Ok(root_anchor_entry_address)
+            }
+        },
+        Some(address) => Ok(address),
     }
+
 }
 
 // Add an anchor with a type
@@ -58,17 +76,11 @@ pub fn create_anchor(anchor_type: String, anchor_text: String) -> ZomeApiResult<
         }.into()
     );
     let anchor_address = hdk::commit_entry(&anchor_entry)?;
-    // TODO put root_anchor address into lazy static
-    hdk::link_entries(&root_anchor()?, &anchor_address, ROOT_ANCHOR_LINK_TO, "")?;
+    hdk::link_entries(&(root_anchor()?), &anchor_address, ROOT_ANCHOR_LINK_TO, "")?;
     Ok(anchor_address)
 }
 
-pub fn get_anchor(anchor_address: Address) -> ZomeApiResult<Option<Entry>> {
-    hdk::get_entry(&anchor_address)
-}
-
 pub fn get_anchors() -> ZomeApiResult<Vec<Address>> {
-    // TODO lazy static
     let root_anchor_entry_address = root_anchor()?;
     Ok(hdk::get_links(&root_anchor_entry_address, LinkMatch::Exactly(ROOT_ANCHOR_LINK_TO), LinkMatch::Any)?.addresses().to_owned())
 }
