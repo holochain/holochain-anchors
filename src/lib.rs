109c9fd902083c6db150d9f9c51dcfed283447b2
Philip Beadle <philip.beadle@live.com.au>
2020-07-30 19:34:19 +1000

    New functions

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ use hdk::{
 };
 use serde_derive::{Deserialize, Serialize};
 use hdk::prelude::*;
-use hdk::api::AGENT_ADDRESS;
 pub mod defs;
 pub use defs::*;
 
@@ -24,7 +23,6 @@ pub struct Anchor {
 /// If the anchor already exists then it will use the existing anchor.
 /// If not it will commit it and check to see if it'd Anchor Type and Root anchor need to be committed too.
 pub fn anchor(anchor_type: String, anchor_text: String) -> ZomeApiResult<Address> {
-    agent_anchor()?;
     let anchor_entry = Anchor::new(anchor_type.clone(), Some(anchor_text.clone())).entry();
     let anchor_address = anchor_entry.address();
     if let Ok(None) = hdk::get_entry(&anchor_entry.address()) {
@@ -88,6 +86,15 @@ pub fn list_anchor_tags(anchor_type: String) -> ZomeApiResult<Vec<String>> {
     .collect())
 }
 
+pub fn link_anchors(anchor_from_type: String, anchor_from_text: String, anchor_type: String, anchor_text: String) -> ZomeApiResult<Address> {
+    hdk::link_entries(&anchor(anchor_from_type, anchor_from_text.clone())?, &anchor(anchor_type, anchor_text)?, ANCHOR_LINK_TYPE, &anchor_from_text)
+}
+
+pub fn bi_directional_link_anchors(anchor_1_type: String, anchor_1_text: String, anchor_2_type: String, anchor_2_text: String) -> ZomeApiResult<Address> {
+    hdk::link_entries(&anchor(anchor_1_type.clone(), anchor_1_text.clone())?, &anchor(anchor_2_type.clone(), anchor_2_text.clone())?, ANCHOR_LINK_TYPE, &anchor_1_text.clone())?;
+    hdk::link_entries(&anchor(anchor_2_type.clone(), anchor_2_text.clone())?, &anchor(anchor_1_type.clone(), anchor_1_text.clone())?, ANCHOR_LINK_TYPE, &anchor_2_text.clone())
+}
+
 fn check_parent(anchor_type: String) -> ZomeApiResult<Address> {
     let anchor_type_entry = Anchor::new(anchor_type.clone(), None).entry();
     let anchor_type_address = anchor_type_entry.address();
@@ -128,33 +135,4 @@ impl Anchor {
     fn entry(self) -> Entry {
         Entry::App(ANCHOR_TYPE.into(), self.into())
     }
-}
-
-fn agent_anchor() -> ZomeApiResult<Address> {
-    let anchor_type = "Agents".to_string();
-    let anchor_text = AGENT_ADDRESS.to_string();
-    let anchor_entry = Anchor::new(anchor_type.clone(), Some(anchor_text.clone())).entry();
-    let anchor_address = anchor_entry.address();
-    if let Ok(None) = hdk::get_entry(&anchor_entry.address()) {
-        hdk::commit_entry(&anchor_entry)?;
-        let anchor_type_address = check_parent(anchor_type)?;
-        hdk::link_entries(
-            &anchor_type_address,
-            &anchor_address,
-            ANCHOR_LINK_TYPE,
-            &anchor_text,
-        )?;
-    }
-    Ok(anchor_address)
-}
-
-/// Gives a list of all link tags from an anchor type (same as the anchor_text value)
-pub fn list_agents() -> ZomeApiResult<Vec<String>> {
-    let anchor_type = "Agents".to_string();
-    let anchor_type_entry = Anchor::new(anchor_type.clone(), None).entry();
-    let anchor_type_address = anchor_type_entry.address();
-    Ok(hdk::get_links(&anchor_type_address, LinkMatch::Exactly(ANCHOR_LINK_TYPE), LinkMatch::Any)?.links()
-    .iter()
-    .map(|link| link.tag.clone())
-    .collect())
 }
